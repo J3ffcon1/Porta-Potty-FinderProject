35f7e6d55969cb280acb3f1ea09ae9bc6c0d12ca
Langston McClarron <langstunned@gmail.com>
2017-09-17 13:45:25 -0700

    edited google map icon

--- googlemap.js
+++ googlemap.js
@@ -1,35 +1,54 @@
 var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map_holder'), {
-        center: {lat: 45.523 , lng: -122.681 },
-        zoom: 16
+    center: {lat: 45.523 , lng: -122.681 },
+    zoom: 16,
   });
-infoWindow = new google.maps.InfoWindow;
 
-if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+  map.setOptions({
+    styles:[
+      {
+        featureType: 'poi.business',
+        stylers: [{visibility: 'off'}]
+      },
+      // {
+      //   featureType: 'transit',
+      //   elementType: 'labels.icon',
+      //   stylers: [{visibility: 'off'}]
+      // }
+    ]
+  })
 
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Find a seat near you');
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        }
-      }
+  infoWindow = new google.maps.InfoWindow;
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Find a seat near you');
+      infoWindow.open(map);
+      map.setCenter(pos);
+      var marker = new google.maps.Marker({
+        position: pos,
+        map:map
+      });
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+    'Error: The Geolocation service failed.' :
+    'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+  }
