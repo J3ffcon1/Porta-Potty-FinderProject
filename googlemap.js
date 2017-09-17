a1d4356421898f3a98ba0e20ab5d7eb5268452d1
Langston McClarron <langstunned@gmail.com>
2017-09-17 11:04:42 -0700

    geolocation map working

--- googlemap.js
+++ googlemap.js
@@ -1,9 +1,35 @@
-var map;
+var map, infoWindow;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map_holder'), {
         center: {lat: 45.523 , lng: -122.681 },
         zoom: 16
   });
-}
-// async defer
-// src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBxM8Bh8A5SsoWYt1SL_LUpC3GMrCgzPzo&callback=initMap";
+infoWindow = new google.maps.InfoWindow;
+
+if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Find a seat near you');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
