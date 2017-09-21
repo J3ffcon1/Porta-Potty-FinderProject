f2cc4bdbc8e07d4603c8268b6ffd2e14fd1fbfe5
J3ffcon1 <Jrlone1990@gmail.com>
2017-09-20 19:38:01 -0700

    Parsed latitude and longitude

--- localStorage.js
+++ localStorage.js
@@ -1,7 +1,10 @@
 function getCoordinates() {
   var longlat = localStorage.getItem('Coordinates');
 
-  document.getElementById('position').value = longlat;
+  var longlatnumber = JSON.parse(longlat);
+  console.log(longlatnumber);
+  document.getElementById('position1').value = longlatnumber.lat;
+  document.getElementById('position2').value = longlatnumber.lng;
 }
 
 window.addEventListener("load", getCoordinates);
